f01ecb44008280cc970c4df1f30d8654b9e584dc
SVKUZNETSOV <svk214@yandex.ru>
2019-05-15 19:08:11 +0300

    Python_study

--- Lab1.3/SNMP.py
+++ Lab1.3/SNMP.py
@@ -11,6 +11,14 @@ result = getCmd(SnmpEngine(),
 for i in result:
 	for a in i[3]:
 		print(a)
+result2 = nextCmd(SnmpEngine(),
+        CommunityData(community_name, mpModel=0),
+	    UdpTransportTarget((ipaddr_string, port_int)),
+		ContextData(),
+		ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.2')), lexicographicMode=False)
+for i in result2:
+	for a in i[3]:
+		print(a)
 #result2 = nextCmd (('1.3.6.1.2.1.2.2.1.2'), lexicographicMode=False)
 #snmp_object = ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)
 #snmp_object = ObjectIdentity('1.3.6.1.2.1.2.2.1.2')
